29711453384df13e288f065cac1acb791062f4b6
Router433 <nugroho31.com@gmail.com>
2017-07-06 09:18:26 +0700

    Create About.sh

---
+++ About.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN v1.0" 
+echo -e ""
+echo -e "For Debian 7 32 bit & 64 bit"
+echo -e "For VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "./Bryan"
+echo -e "https://www.solocyberarmy.net"
+echo -e "Thanksyou VeryMuch"
+echo -e ""
