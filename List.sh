7b7cf4dcc0e72fcc6c6c3aac26c01e505cb4cfaf
Router433 <nugroho31.com@gmail.com>
2017-07-06 08:38:50 +0700

    Create List.sh

---
+++ List.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#List
+
+echo -e "* /list      		 : Displays a list of commands"
+echo -e "* /newmembers 		 : Create SSH and OpenVPN accounts"
+echo -e "* /trial     		 : Create Account trial"
+echo -e "* /remove     	 	 : Remove User SSH and OpenVPN"
+echo -e "* /check       	 : check user login"
+echo -e "* /members    	     : check members SSH and OpenVPN"
+echo -e "* /maintance    	 : restart service dropbear, webmin"
+echo -e "                      squid3, openvpn and ssh"
+echo -e "* /reboot    		 : reboot VPS"
+echo -e "* /speedtest 		 : speedtest VPS"
+echo -e "* /information      : Display system information"
+echo -e "* /about     		 : Auto install script info"
+echo -e "* /exit      		 : Out of Putty / Connectbot /"
+echo -e "              			JuiceSSH"
+echo -e "./Bryan"
+echo -e ""
